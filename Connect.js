62cfa923ccac856aab8dd99898627fe1c8173a45
Joel Martinez (Texas State account) <42950020+JMartinez0404@users.noreply.github.com>
2018-10-03 21:44:19 -0500

    Update Connect.js

--- Connect.js
+++ Connect.js
@@ -80,18 +80,6 @@ function echo (target, context, params) {
     }
 }
 
-// Function called when the "haiku" command is issued:
-function haiku (target, context) {
-    // Generate a new haiku:
-    haikudos((newHaiku) => {
-        // Split it line-by-line:
-        newHaiku.split('\n').forEach((h) => {
-            // Send each line separately:
-            sendMessage(target, context, h)
-        })
-    })
-}
-
 // Helper function to send the correct type of message:
 //Know that Commands do not run in Whisper
 function sendMessage (target, context, message) { 
