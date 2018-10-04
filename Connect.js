30318a1ae366fb96570510ab486cf6170c879f21
wcj1 <36248206+wcj1@users.noreply.github.com>
2018-10-04 13:22:43 -0500

    added my functions givepts and doom back

--- Connect.js
+++ Connect.js
@@ -168,6 +168,27 @@ function slap(target, context, slapee) {
 	
 }
 
+
+//Function called when the "doom" command is issued:
+//function created by wcj1
+function doom(target, context, params) {
+    var thing = (context);
+
+    if(params.length)
+        var msg = params.join(' ');
+    
+    //Prints return message
+    sendMessage(target, context, 'Yes, Doom will run on anything, even on a ' + msg );
+}
+
+//Function called when "givepts" command is issued:
+//Function created by wcj1
+function givepts(target, context) {
+    var pts = Math.floor((Math.random()+1 ) * 10);
+    sendMessage(target, context, context.username + ' got ' + pts + ' points. YAY!');
+    //sendMessage(target, context, user.username );
+}
+
 // Helper function to send the correct type of message:
 //Know that Commands do not run in Whisper
 function sendMessage (target, context, message) { 
@@ -177,3 +198,4 @@ function sendMessage (target, context, message) {
         client.say(target, message)
     }
 }
+
