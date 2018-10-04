b0746de22fb17ae54aca84a83c4096d781137f07
JMartinez0404 <42950020+JMartinez0404@users.noreply.github.com>
2018-10-03 19:01:07 -0500

    Deleted my function and moved it to my branch
    gamble function is now in the Joelshare branch in the Joel's Commands.js file

--- Connect.js
+++ Connect.js
@@ -15,7 +15,7 @@ let opts = {
 }
 
 // These are the commands the bot knows (defined below):
-let knownCommands = { echo, haiku, coinflip, gamble, slap, doom, givepts } //add new commands to this list
+let knownCommands = { echo, haiku, doom, givepts } //add new commands to this list
 // Create a client with our options:
 let client = new tmi.client(opts)
 
@@ -90,30 +90,6 @@ function haiku (target, context) {
             sendMessage(target, context, h)
         })
     })
-}
-
-// Function called when the "gamble" command is issued:
-function gamble(target, context, params) {
-    var coin = Math.floor(Math.random() * 2);
-
-    //takes in bet input
-    if (params.length)
-        var msg = params.join(' ');
-
-    if (coin == 0)
-        coin = 'tails';
-    else
-        coin = 'heads';
-
-    // Prints gamble messages;
-    if (coin == 'tails' && coin == msg)
-        sendMessage(target, context, 'You bet on Tails and you won the bet (somehow). You won 50 coins');
-    else if (coin == 'heads' && coin == msg)
-        sendMessage(target, context, 'You bet on Heads and you won the bet (somehow). You won 50 coins');
-    else if (coin == 'tails' && !(coin == msg))
-        sendMessage(target, context, 'You bet on Heads and you lost the bet. You lost 100 coins..boohoo');
-    else if (coin == 'heads' && !(coin == msg))
-        sendMessage(target, context, 'You bet on Tails and you lost the bet. You lost 100 coins..boohoo');
 }
 
 //Function called when the "doom" command is issued:
