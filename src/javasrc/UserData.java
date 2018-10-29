3a5dfebb0e5d275119731f03ab4d49f6ad66ea37
lts25 <42810743+lts25@users.noreply.github.com>
2018-10-29 12:02:32 -0500

    literally nothing so far
    things have tried to begin

---
+++ src/javasrc/UserData.java
@@ -0,0 +1,56 @@
+package GetRichTwitch;
+
+public class UserData {
+	private String username;
+	private int points;
+	private boolean isMod;
+	private boolean isSub;
+	private String badges;
+	
+	public void setusername(String name) {
+		
+	}
+	
+	public String getusername() {
+		return this.username
+	}
+	
+	public void setpoints(int num) {
+
+	}
+	public void addpoints(int num) {
+
+	}	
+	public void subpoints(int num) {
+		
+	}
+	
+	public String getpoints() {
+		return this.points
+	}
+	
+	public void setisMod(boolean mod) {
+		
+	}
+	
+	
+	public String getisMod() {
+		return this.isMod
+	}
+	
+	public void setisSub(boolean sub) {
+		
+	}
+	
+	public String getusername() {
+		return this.isSub
+	}
+	
+	public void setbadges(string bagers) {
+		
+	}
+	
+	public String getbadges() {
+		return this.badges
+	}
+}
