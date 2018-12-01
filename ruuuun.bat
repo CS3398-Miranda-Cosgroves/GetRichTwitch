164c0271a2d8c9632be0ac02136a33d543ebe4f8
lts25 <42810743+lts25@users.noreply.github.com>
2018-12-01 13:06:38 -0600

    installs node apis if they are needed
    wont install node

--- ruuuun.bat
+++ ruuuun.bat
@@ -1,2 +1,27 @@
+IF EXIST "%~dp0\node_modules\haikudos" GOTO hai
+call npm i haikudos
+
+:hai
+
+IF EXIST "%~dp0\node_modules\tmi.js" GOTO tmi
+call npm i tmi.js
+:tmi
+
+IF EXIST "%~dp0\node_modules\get-video-id" GOTO vid
+call npm i get-video-id
+:vid
+
+IF EXIST "%~dp0\node_modules\googleapis" GOTO goog
+call npm i googleapis
+:goog
+
+IF EXIST "%~dp0\node_modules\google-auth-library" GOTO goa
+call npm i google-auth-library
+:goa
+
+IF EXIST "%~dp0\node_modules\youtube-info" GOTO you
+call npm i youtube-info
+:you
+
 node src/JS/connect.js
 pause
